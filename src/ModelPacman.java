39f695f39d9493002d55e71ecb9d01c6074cd321
Bestarisqya <bestarisqya10@gmail.com>
2022-05-19 17:01:52 +0700

    Initial commit

---
+++ src/ModelPacman.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ModelPacman {
+    private static void main(String[]args) {
+        String JumlahPemain;
+        String LebarLayar;
+        String TinggiLayar;
+        String JumlahHantu;
+        String JumlahLivePacman;
+        String EntityScore;
+        String JumlahNyawa;
+
+        JumlahPemain ="3";
+        LebarLayar ="6 cm";
+        TinggiLayar ="10 cm";
+        JumlahHantu ="5";
+        JumlahLivePacman ="1";
+        EntityScore ="10";
+        JumlahNyawa ="3";
+        Scanner inputUser = new Scanner(System.in);
+        System.out.println("Menggerakkan Pacman");
+        System.out.println("Memindahkan Posisi Pacman");
+
+
+    }
+}
---
+++ src/Pacman.java
@@ -0,0 +1,18 @@
+public class Pacman {
+    public String JumlahPemain;
+    public String LebarLayar;
+    public String TinggiLayar;
+    public String JumlahHantu;
+    public String JumlahLivePacman;
+    public String EntityScore;
+    public String JumlahNyawa;
+
+    public Pacman(String jumlahHantu, String lebarLayar, String tinggiLayar, String jumlahLivePacman, String entityScore,String jumlahNyawa, String jumlahPemain ) {
+        this.JumlahHantu = jumlahHantu;
+        this.LebarLayar = lebarLayar;
+        this.TinggiLayar = tinggiLayar;
+        this.JumlahLivePacman = jumlahLivePacman;
+        this.EntityScore = entityScore;
+        this.JumlahNyawa = jumlahNyawa;
+        this.JumlahPemain = jumlahPemain;
+    }
